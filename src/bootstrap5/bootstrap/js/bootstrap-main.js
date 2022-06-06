443daa82d7405aeb08f87991c1600196008a9f26
Mayur~ <=>
2022-06-06 22:34:40 +0530

    Major update for round 2

--- src/bootstrap5/bootstrap/js/bootstrap-main.js
+++ src/bootstrap5/bootstrap/js/bootstrap-main.js
@@ -257,10 +257,34 @@
       },
 
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 1,
       }
     }
   });
+
+
+//HERO SLIDER
+
+  var swiper = new Swiper(".sliderFeaturedPosts", {
+    spaceBetween: 0,
+    speed: 500,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+  });
+
 
   /**
    * Animation on scroll
